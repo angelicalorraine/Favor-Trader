27f5bc9f7b201402c5bf026c77750352169fcc2e
Angelica Hernandez <gel.lorraine@gmail.com>
2021-06-19 15:05:12 -0700

    Initial Commit - Structure

---
+++ routes/api/api3.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { Model3 } = require('../../models/model3');
+
+// CREATE a
+router.post('/', async (req, res) => {
+
+});
+
+// DELETE a
+router.delete('/:id', async (req, res) => {
+
+});
+
+module.exports = router;
