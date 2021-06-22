a3ec494cf198b656ab2ccbc0463670bdecbc66a5
Angelica Hernandez <gel.lorraine@gmail.com>
2021-06-22 15:18:07 -0700

    MVC updates

--- controllers/api/api3.js
+++ controllers/api/api3.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Model3 } = require('../../models/model3');
+//const withAuth = require('../../utils/auth');
 
 // CREATE a
 router.post('/', async (req, res) => {
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
 
