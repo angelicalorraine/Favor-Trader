9d2fc642fb84b66847ac72430db9a1f3430c3152
Angelica Hernandez <gel.lorraine@gmail.com>
2021-06-25 15:23:10 -0700

    Log in route - Profile page

--- controllers/api/UserRoute.js
+++ controllers/api/UserRoute.js
@@ -1,7 +1,66 @@
 const router = require('express').Router();
-const { User, Favor, Offer, Trade } = require('../../models');
-//const withAuth = require('../../utils/auth');
+const { User, Favor } = require('../../models');
 
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        // Set up sessions with a 'loggedIn' variable set to `true`
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.loggedIn = true;
+
+            res.status(200).json(UserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 router.get('/', async (req, res) => {
     try {
@@ -29,10 +88,17 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// CREATE a
-router.post('/', async (req, res) => {
-
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
+
+
 
 // DELETE a
 router.delete('/:id', async (req, res) => {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,13 @@
 const router = require('express').Router();
-const { Favor, Offer, Trade, User } = require('../models');
-//For Auth
-//const withAuth = require('../utils/auth');
-
-// router.get('/', function (req, res, next) {
-//   res.render('homePage', { title: 'Express' });
-// });
+const { Favor, Trade, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const favorData = await Favor.findAll({
       include: [
-        { model: User }, { model: Offer }, { model: Trade }
+        { model: User }, { model: Trade }
       ],
     });
 
@@ -28,6 +23,57 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/favor/:id', async (req, res) => {
+  try {
+    const favorData = await Favor.findByPk(req.params.id, {
+      include: [
+        {
+          model: User
+        },
+      ],
+    });
+
+    const favors = favorData.get({ plain: true });
+
+    res.render('profile', {
+      ...favors,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Favor }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
 
 
 
